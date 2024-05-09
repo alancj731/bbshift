2131481ace6b235bede13542ee50d69854dfa84c
alancj731 <alanchenjian@gmail.com>
2024-05-08 22:34:17 -0500

    login done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+'use client'
 import DashBoard from "@/components/DashBoard";
+import HomePage from "@/components/Home";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <DashBoard />
+    <main className="flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-800">
+      <HomePage />
     </main>
   );
 }
---
+++ src/app/shift/page.tsx
@@ -0,0 +1,9 @@
+import DashBoard from "@/components/DashBoard";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <DashBoard />
+    </main>
+  );
+}
